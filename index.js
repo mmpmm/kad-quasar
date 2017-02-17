a808b88f15f5adbabf58da6ad09b451463c47a19
Gordon Hall <gordon@gordonwritescode.com>
2017-02-17 14:53:26 -0500

    update to use atbf lib

--- index.js
+++ index.js
@@ -6,7 +6,6 @@
 
 /** {@link NodeDecorator} */
 module.exports.NodeDecorator = require('./lib/node-decorator');
+
 /** {@link Protocol} */
 module.exports.Protocol = require('./lib/quasar');
-/** {@link BloomFilter} */
-module.exports.BloomFilter = require('./lib/bloomfilter');
