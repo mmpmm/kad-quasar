021d788d98be6942d35794c00ff9b811042f4a68
Gordon Hall <gordon@gordonwritescode.com>
2017-03-20 15:25:07 -0400

    test suite for rules

---
+++ test/plugin-quasar.unit.js
@@ -0,0 +1,76 @@
+'use strict';
+
+const { expect } = require('chai');
+const sinon = require('sinon');
+const QuasarPlugin = require('../lib/plugin-quasar');
+
+
+describe('QuasarPlugin', function() {
+
+  describe('@constructor', function() {
+
+
+
+  });
+
+  describe('@property neighbors', function() {
+
+
+
+  });
+
+  describe('@method quasarPublish', function() {
+
+
+
+  });
+
+  describe('@method quasarSubscribe', function() {
+
+
+
+  });
+
+  describe('@method pullFilters', function() {
+
+
+
+  });
+
+  describe('@method pullFilterFrom', function() {
+
+
+
+  });
+
+  describe('@method pushFilters', function() {
+
+
+
+  });
+
+  describe('@method pushFilterTo', function() {
+
+
+
+  });
+
+  describe('@method isSubscribedTo', function() {
+
+
+
+  });
+
+  describe('@hasNeighborSubscribedTo', function() {
+
+
+
+  });
+
+  describe('@private _getRandomContact', function() {
+
+
+
+  });
+
+});
